990464310f6954d70c6c8eb242c1217458df7e2a
Simon Corless <simon@sico.co.uk>
2015-08-16 12:47:39 +0100

    Fixed to use default form field value.
    Removed ability to set default value and use form field value instead.

--- src/jquery.spinner.js
+++ src/jquery.spinner.js
@@ -14,12 +14,12 @@
 ;(function ($) {
   $.fn.spinner = function (opts) {
     return this.each(function () {
-      var defaults = {value:0, min:0}
+      var defaults = {min:0}
       var options = $.extend(defaults, opts)
       var keyCodes = {up:38, down:40}
       var container = $('<div></div>')
       container.addClass('spinner')
-      var textField = $(this).addClass('value').attr('maxlength', '2').val(options.value)
+      var textField = $(this).addClass('value').attr('maxlength', '2')
         .bind('keyup paste change', function (e) {
           var field = $(this)
           if (e.keyCode == keyCodes.up) changeValue(1)
@@ -32,7 +32,7 @@
       var decreaseButton = $('<button class="decrease">-</button>').click(function () { changeValue(-1) })
 
       validate(textField)
-      container.data('lastValidValue', options.value)
+      container.data('lastValidValue', textField.val())
       textField.before(decreaseButton)
       textField.after(increaseButton)
 
